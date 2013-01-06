09bfc7ea91688e71d4ced06f41d772564c74c629
NARUSE, Yui <naruse@airemix.jp>
2013-01-07 01:49:27 +0900

    Add mass assign support

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,10 +1,11 @@
 class Server < ActiveRecord::Base
   validates :name, :length => { :in => 3..30 }
-  validates :arch, :inclusion => { :in => %w(x86 x64),
+  validates :arch, :inclusion => { :in => %w(- x86 x64),
         :message => "%{value} is not a valid arch" }
-  validates :os, :length => { :in => 3..20 }
-  validates :version, :length => { :in => 3..20 }
+  validates :os, :length => { :in => 1..20 }
+  validates :version, :length => { :in => 1..20 }
   validates :uri, :length => { :in => 20..200 }
+  attr_accessible :name, :arch, :os, :version, :uri
 
   def recent_uri(branch)
     [uri.sub(/\/$/, ''), 'ruby-' + branch, 'recent.html'].join('/')
