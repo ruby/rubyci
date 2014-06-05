5f58e83a7b3c7e7e0934b911ae226ffee40d6824
Tanaka Akira <akr@fsij.org>
2014-06-05 18:37:53 +0900

    Merge test files.

--- test/chkbuild-ruby-info/test_misc.rb
+++ test/chkbuild-ruby-info/test_misc.rb
@@ -42,4 +42,27 @@ End
 ]
 End
   end
+
+  def test_td
+    exc = nil
+    out, err = capture_stdout_stderr {
+      cb = ChkBuildRubyInfo.new(<<'End')
+== ruby-trunk # 2010-12-02T16:51:01+09:00
+End
+      cb.common_hash = {
+        server_id: 1,
+        depsuffixed_name: 'ruby-trunk',
+        epoch: 1291276261,
+        revision: 4200,
+      }
+      cb.convert_to_td
+    }
+    assert_equal(<<'End', out)
+@[chkbuild.section_start] {"type":"section_start","secname":"ruby-trunk","start_time":"2010-12-02T16:51:01+09:00","server_id":1,"depsuffixed_name":"ruby-trunk","epoch":1291276261,"revision":4200}
+@[chkbuild.depsuffixed_name] {"type":"depsuffixed_name","depsuffixed_name":"ruby-trunk","server_id":1,"depsuffixed_name":"ruby-trunk","epoch":1291276261,"revision":4200}
+@[chkbuild.suffixed_name] {"type":"suffixed_name","suffixed_name":"ruby-trunk","server_id":1,"depsuffixed_name":"ruby-trunk","epoch":1291276261,"revision":4200}
+@[chkbuild.target_name] {"type":"target_name","target_name":"ruby","server_id":1,"depsuffixed_name":"ruby-trunk","epoch":1291276261,"revision":4200}
+@[chkbuild.build] {"type":"build","depsuffixed_name":"ruby-trunk","suffixed_name":"ruby-trunk","target_name":"ruby","server_id":1,"depsuffixed_name":"ruby-trunk","epoch":1291276261,"revision":4200}
+End
+  end
 end
