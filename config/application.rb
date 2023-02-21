eae3489ca46a29448f058bb26853d32d897c32aa
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-02-21 14:03:39 +0900

    Use Rails 7.0 default configuration

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Rubyci
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
