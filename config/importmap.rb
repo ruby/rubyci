a488ab49bbc440537beb05d8ab678c1cbae8e7b6
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-03-02 12:50:47 +0900

    bin/importmap unpin jquery and jquery-ujs

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.3/dist/jquery.js"
-pin "jquery-ujs", to: "https://ga.jspm.io/npm:jquery-ujs@1.2.3/src/rails.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
